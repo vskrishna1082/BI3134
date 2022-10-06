7b8358d47b1344b40b6a3e73a6662fdc83bb2d1c
V S Krishna <krishnaiiserp@gmail.com>
2022-10-06 19:25:30 +0530

    Add primitive support for multiple best-fits

--- sequence_alignment/1_dyn_prog.py
+++ sequence_alignment/1_dyn_prog.py
@@ -7,9 +7,9 @@ import numpy as np
 from collections import deque
 
 # Provide a sequence string
-seq_a= "AATATTATATCCT"
-seq_b= "ATgtacatcct"
-
+seq_a= "ATGTATGCTGCTGCATTGTCTGATCG"
+seq_b= "AATGTAATGCGCTGTCGATCGTAGCT"
+alignments=[]
 # Define a Substitution Matrix and Gap Penality
 subs_matrix = np.array([[2,0,1,0],[0,2,0,1],[1,0,2,0],[0,1,0,2]]) 
 gp = 4 # matrix generation fails when gp=0
@@ -23,7 +23,8 @@ def numSeq(x):
                 return idx
                 break # stop at first match
     return list(map(nuNum, [*x.upper()]))
-
+seq1 = numSeq(seq_a)
+seq2 = numSeq(seq_b)
 # Define a function that maximizes score and returns (score,traceback)
 def maxScoreBack(score_NW, score_N, score_W, subs_score):
     tBack=[0,0,0] # define empty traceback
@@ -71,37 +72,87 @@ def dyn_prog_matrix(seq1, seq2):
     return(st_m)
 
 # Trace best alignment from scoring and traceack matrices
-def trace_alignment(st_matrix, seq1, seq2):
+def trace_alignment(st_matrix):
     a_seq1 = deque() # aligned sequences as lists
     a_seq2 = deque()
-    pos_x = len(seq1)
-    pos_y = len(seq2)
-    curr_tback = st_matrix[1][pos_y][pos_x]
+    x = np.size(st_matrix,axis=2)-1
+    y =  np.size(st_matrix,axis=1)-1
+    curr_tback = st_matrix[1][y][x]
+
+    def move_diagonal(al_seq1,al_seq2, pos_x, pos_y):
+        al_seq1.appendleft(nuLib[seq1[pos_x-1]])
+        al_seq2.appendleft(nuLib[seq2[pos_y-1]])
+        pos_y+=-1
+        pos_x+=-1
+        tback = st_matrix[1][pos_y][pos_x]
+        return (tback,pos_x,pos_y)
+
+    def move_left(al_seq1,al_seq2, pos_x, pos_y):
+        pos_x+=-1
+        al_seq1.appendleft(nuLib[seq1[pos_x]])
+        al_seq2.appendleft('-')
+        tback = st_matrix[1][pos_y][pos_x]
+        return (tback,pos_x,pos_y)
+
+    def move_up(al_seq1,al_seq2, pos_x, pos_y):
+        pos_y+=-1
+        al_seq1.appendleft('-')
+        al_seq2.appendleft(nuLib[seq2[pos_y]])
+        tback = st_matrix[1][pos_y][pos_x]
+        return (tback,pos_x,pos_y)
+
+    def branch_diagonal():
+        branch = [a_seq1.copy(),a_seq2.copy()]
+        branch[0].appendleft(nuLib[seq1[x-1]])
+        branch[1].appendleft(nuLib[seq2[y-1]])
+        sub_align = trace_alignment(st_matrix[:,:y,:x])
+        branch[0]=sub_align[0]+branch[0] 
+        branch[1]=sub_align[1]+branch[1]
+        alignments.append(tuple(branch))
+
+    def branch_left():
+        branch = [a_seq1.copy(),a_seq2.copy()]
+        branch[0].appendleft(nuLib[seq1[x-1]])
+        branch[1].appendleft('-')
+        sub_align = trace_alignment(st_matrix[:,:y+1,:x])
+        branch[0]=sub_align[0]+branch[0] 
+        branch[1]=sub_align[1]+branch[1]
+        alignments.append(tuple(branch))
+
     while curr_tback != 0:
-        #print(f"{pos_x},{pos_y} tb= {curr_tback}") # debugging
+#        print(f"{x},{y} tb= {curr_tback}") # debugging
         if curr_tback == 4:
-            a_seq1.appendleft(nuLib[seq1[pos_x-1]])
-            a_seq2.appendleft(nuLib[seq2[pos_y-1]])
-            pos_y+=-1
-            pos_x+=-1
-            curr_tback = st_matrix[1][pos_y][pos_x]
-        elif curr_tback == 1 or curr_tback == 5: # gap in second sequence
-            pos_x+=-1
-            a_seq1.insert(0, nuLib[seq1[pos_x]])
-            a_seq2.insert(0, '-')
-            curr_tback = st_matrix[1][pos_y][pos_x]
-        elif curr_tback == 2 or curr_tback == 3 or curr_tback == 6 or curr_tback == 7: # gap in first sequence
-            pos_y+=-1
-            a_seq1.insert(0, '-')
-            a_seq2.insert(0, nuLib[seq2[pos_y]])
-            curr_tback = st_matrix[1][pos_y][pos_x]
-    return ''.join(a_seq1)+"\n"+''.join(a_seq2)
+            (curr_tback,x,y) = move_diagonal(a_seq1,a_seq2,x,y)
+
+        elif curr_tback == 1: 
+            (curr_tback,x,y) = move_left(a_seq1,a_seq2,x,y)
+
+        elif curr_tback == 2:
+            (curr_tback,x,y) = move_up(a_seq1,a_seq2,x,y)
+
+        elif curr_tback == 3:
+            branch_left()
+            (curr_tback,x,y) = move_up(a_seq1,a_seq2,x,y)
+
+        elif curr_tback == 5:
+            branch_diagonal()
+            (curr_tback,x,y) = move_left(a_seq1,a_seq2,x,y)
+
+        elif curr_tback == 6:
+            branch_diagonal()
+            (curr_tback,x,y) = move_up(a_seq1,a_seq2,x,y)
+
+        elif curr_tback == 7:
+            branch_diagonal()
+            branch_left()
+            (curr_tback,x,y) = move_up(a_seq1,a_seq2,x,y)
+    return (a_seq1,a_seq2)
 
 def mytestfunc():
-    nseq_a = numSeq(seq_a)
-    nseq_b = numSeq(seq_b)
-    stmatrix=dyn_prog_matrix(nseq_a,nseq_b)
-    aligned=trace_alignment(stmatrix,nseq_a,nseq_b)
-    print(aligned)
-    print("Score: "+str(stmatrix[0][len(nseq_b)][len(nseq_a)]))
+    stmatrix=dyn_prog_matrix(seq1,seq2)
+    aligned=trace_alignment(stmatrix)
+    alignments.append(aligned)
+    for idx, i in enumerate(alignments):
+        print('\nSequence '+str(idx+1)+':\n'+''.join(i[0])+'\n'+''.join(i[1]))
+    print("Score: "+str(stmatrix[0][len(seq2)][len(seq1)]))
 print(f"Time taken: {timeit.timeit(mytestfunc, number=1)}")
