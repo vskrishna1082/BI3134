c67d91aa46c3205e64623bc90ea7a2114df14b03
V S Krishna <krishnaiiserp@gmail.com>
2022-10-14 16:02:30 +0530

    read negative and double-digit gap penalities from csv

--- sequence_alignment/symloader.py
+++ sequence_alignment/symloader.py
@@ -2,19 +2,19 @@
 # Symbol Loader for sequence alignment (dynamic programming)
 import pandas as pd
 import os
+import re
 
 class LoadSymbols:
     """Get substitution scores and gap penality from file"""
     def __init__(self, matrix_fileName):
         matrix_file = os.path.join('substitution_matrices', matrix_fileName)
-        f = open(matrix_file, 'r')
-        f.seek(14)
+        f = open(matrix_file, 'rb')
         df = pd.read_csv(matrix_file,
                          sep=',',
                          comment='#',
                          index_col=0,
                          skipinitialspace=True
                          )
-        self.gapPenality = int(f.read(1))
+        self.gapPenality =int(re.search('-?\d+', str(f.readline()))[0])
         self.symLibrary = list(df.columns)
         self.subScores = df.to_numpy()
